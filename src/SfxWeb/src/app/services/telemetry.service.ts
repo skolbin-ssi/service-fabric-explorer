17558b92b7680e8814167bc94e5bebb916e9a1b3
jeffj6123 <jeffrey.jarry@gmail.com>
2020-02-26 16:42:40 -0800

    telemetry basics.

--- src/SfxWeb/src/app/services/telemetry.service.ts
+++ src/SfxWeb/src/app/services/telemetry.service.ts
@@ -1,15 +1,62 @@
 import { Injectable } from '@angular/core';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import { Router, NavigationEnd, ActivatedRoute, ActivationEnd } from '@angular/router';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TelemetryService {
 
-  constructor() { }
+  appInsights: ApplicationInsights;
+  telemetryEnabled: boolean = true;
+  static readonly localStorageKey = "sfx-telemetry-enabled";
+
+  constructor(public routing: Router, private storage: StorageService) {
+
+    this.telemetryEnabled= this.storage.getValueBoolean(TelemetryService.localStorageKey, true);
+
+    //enable telemetry
+    this.appInsights = new ApplicationInsights({ config: {
+      instrumentationKey: 'YOUR_INSTRUMENTATION_KEY_GOES_HERE',
+      isCookieUseDisabled: true,
+      disableAjaxTracking: true,
+      disableFetchTracking : true,
+      disableTelemetry: !this.telemetryEnabled
+      /* ...Other Configuration Options... */
+    } });
+    this.appInsights.loadAppInsights();
+
+
+    let lastActivationEnd = null;
+    this.routing.events.subscribe( event => {
+      if(event instanceof ActivationEnd) {
+        lastActivationEnd = event;
+      }
+        if(event instanceof NavigationEnd){
+            // console.log(this.activatedRoute.snapshot);
+            const name = lastActivationEnd.snapshot.routeConfig.path;
+            console.log(lastActivationEnd.snapshot.routeConfig.path);
+            this.appInsights.trackPageView({
+              name
+            })
+            lastActivationEnd = null;
+        }
+    })
+   }
 
   trackActionEvent(name: string, source: string, data: any) {
     console.log("track event");
-    //TODO
+  }
+
+  /*
+  set the state for whether telemetry should be enabled.
+  true = enabled
+  */
+  public SetTelemetry(state: boolean) {
+    this.storage.setValue(TelemetryService.localStorageKey, state);
+    this.telemetryEnabled = state;
+    this.appInsights.config.disableTelemetry = !state;
   }
 
 }
--- src/SfxWeb/src/app/shared/component/advanced-option/advanced-option.component.ts
+++ src/SfxWeb/src/app/shared/component/advanced-option/advanced-option.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/services/storage.service';
 import { Constants } from 'src/app/Common/Constants';
+import { TelemetryService } from 'src/app/services/telemetry.service';
 
 @Component({
   selector: 'app-advanced-option',
@@ -11,14 +12,19 @@ export class AdvancedOptionComponent implements OnInit {
 
   status: boolean = false;
 
-  constructor(public storage: StorageService) { }
+  constructor(public storage: StorageService, public telemetryService: TelemetryService) { }
 
   ngOnInit() {
     this.status = this.storage.getValueBoolean(Constants.AdvancedModeKey, false);
+    this.telemetryService.telemetryEnabled
   }
 
   change() {
     this.storage.setValue(Constants.AdvancedModeKey, this.status);
+  }
+
+  telemetryChange() {
+    this.telemetryService.SetTelemetry(this.telemetryService.telemetryEnabled);
   }
 
 }
